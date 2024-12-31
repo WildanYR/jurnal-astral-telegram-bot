5c185e619302fc66464036a4b7b7caa26d21ae11
WildanYR <wildanyahya99@gmail.com>
2024-12-31 22:10:50 +0700

    lint

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,44 +5,70 @@ import { setChatInstanceState } from "../utils/chat_instance.util";
 import { authConst } from "../constants/auth.const";
 import { botConst } from "../constants/bot.const";
 
-export const authInit = async (bot: TelegramBot, chat_id: number, user_id: number) => {
-  await bot.sendMessage(chat_id, 'Silahkan masukkan token')
-  await setChatInstanceState(chat_id, user_id, authConst.state.GET_TOKEN)
-}
+export const authInit = async (
+  bot: TelegramBot,
+  chat_id: number,
+  user_id: number
+) => {
+  await bot.sendMessage(chat_id, "Silahkan masukkan token");
+  await setChatInstanceState(chat_id, user_id, authConst.state.GET_TOKEN);
+};
 
-export const authValidate = async (bot: TelegramBot, chat_id: number, user_id: number, token: string) => {
+export const authValidate = async (
+  bot: TelegramBot,
+  chat_id: number,
+  user_id: number,
+  token: string
+) => {
   if (token !== appConfig.token) {
-    await bot.sendMessage(chat_id, 'Login Gagal, Token Invalid\n\n/cancel untuk membatalkan')
-    return
+    await bot.sendMessage(
+      chat_id,
+      "Login Gagal, Token Invalid\n\n/cancel untuk membatalkan"
+    );
+    return;
   }
-  
-  const authorizeUser = await AuthorizeUser.findOne({where: {user_id}})
+
+  const authorizeUser = await AuthorizeUser.findOne({ where: { user_id } });
   if (authorizeUser) {
-    await bot.sendMessage(chat_id, 'Anda sudah terdaftar sebagai Admin')
-    return
+    await bot.sendMessage(chat_id, "Anda sudah terdaftar sebagai Admin");
+    return;
   }
-  
+
   try {
-    await AuthorizeUser.create({user_id})
-    await bot.sendMessage(chat_id, 'Login Berhasil, Anda telah menjadi Admin')
+    await AuthorizeUser.create({ user_id });
+    await bot.sendMessage(chat_id, "Login Berhasil, Anda telah menjadi Admin");
   } catch (error: any) {
-    console.log('Error ', error.toString())
+    console.log("Error ", error.toString());
   }
-  await setChatInstanceState(chat_id, user_id, botConst.state.START)
-}
+  await setChatInstanceState(chat_id, user_id, botConst.state.START);
+};
 
 export const isAuthUser = async (user_id: number) => {
-  const count = await AuthorizeUser.count({where: {user_id}})
-  return count > 0
-}
+  const count = await AuthorizeUser.count({ where: { user_id } });
+  return count > 0;
+};
 
-export const logout = async (bot: TelegramBot, chat_id: number, user_id: number) => {
-  const authorizeUser = await AuthorizeUser.findOne({where: {user_id}})
+export const logout = async (
+  bot: TelegramBot,
+  chat_id: number,
+  user_id: number
+) => {
+  const authorizeUser = await AuthorizeUser.findOne({ where: { user_id } });
   if (!authorizeUser) {
-    await bot.sendMessage(chat_id, 'Anda belum login sebagai admin')
-    return
+    await bot.sendMessage(chat_id, "Anda belum login sebagai admin");
+    return;
   }
 
-  await authorizeUser.destroy()
-  await bot.sendMessage(chat_id, 'Logout berhasil')
-}
+  await authorizeUser.destroy();
+  await bot.sendMessage(chat_id, "Logout berhasil");
+};
+
+export const unauthorizedMessage = async (
+  bot: TelegramBot,
+  chat_id: number
+) => {
+  await bot.sendMessage(
+    chat_id,
+    "Memerlukan akses Admin, akun anda bukan admin"
+  );
+};
